7354269f7937d7b75181330ae46b04b9d2ae686d
Ajayj007 <ajayjadhav7.skn.comp@gmail.com>
2025-11-14 04:41:35 +0530

    sign-in Completed

---
+++ Fronted/my-react-app/src/pages/signin.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+function Signin(){
+    
+  const[Email, setEmail] = useState('');
+  const[Password, setPassword] = useState(''); 
+    
+  return(
+
+     <div className="container">
+                  
+         <h1 className="page-header">Sign in</h1>
+                    
+        <div className="login-conatainer">
+                
+                <div class="mb-3">
+          <label for="inputEmail4" class="form-label">Email</label>
+          <input type="email" class="form-control" id="inputEmail4" />
+        </div>
+     
+      
+        <div class="mb-3">
+          <label for="inputPassword4" class="form-label">Password</label>
+          <input type="password" class="form-control" id="inputPassword4" />
+        </div>
+   
+
+     
+        <div class="mb-3">
+          <button type="submit" className="btn btn-success">Sign in</button>
+        </div>
+    
+
+     
+        <div class="mb-3">
+          Don't have an account? <a href="">Sign up</a>
+        </div>
+        </div>
+     
+     </div>
+  )
+}
+
+export default Signin;
